8115f9e3137cd3504bd5ba896ff6bb9766f2f540
toni <leesoeun2746@dgu.ac.kr>
2024-08-07 13:41:27 +0900

    🎉Init

---
+++ src/main/java/dongguk/rangers/RangersApplication.java
@@ -0,0 +1,13 @@
+package dongguk.rangers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangersApplication.class, args);
+	}
+
+}
